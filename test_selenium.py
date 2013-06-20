84357198c326a0da95b9548f65ae0344b8c769f9
daniel <git@runops.com>
2013-06-19 22:43:56 -0700

    adding generator example

--- test_selenium.py
+++ test_selenium.py
@@ -23,3 +23,20 @@ class TestBasic(SeleniumTestCase):
         assert 'Selenium' in self.driver.title 
         assert 'Selenium' in results.text
 
+class TestGenerator(SeleniumTestCase):
+
+    def test_search(self):
+        for search_term in ['Python', 'Selenium', 'San Francisco', 'Sauce Labs']:
+            yield self.verify_search, search_term
+
+    def verify_search(self, search_term):
+        self.driver.get('http://duckduckgo.com')
+
+        search_box = self.driver.find_element_by_name('q')
+        search_box.send_keys(search_term)
+        search_box.submit()
+
+        results = self.driver.find_element_by_id('links')
+
+        assert search_term in self.driver.title 
+        assert search_term in results.text
