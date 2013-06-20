925d98c79a7ed36d4bc3833faccd866c2bc4d74d
danielhochman <git@runops.com>
2013-06-20 02:29:26 -0700

    adding Sauce Labs support

--- basic_selenium_framework/testcases.py
+++ basic_selenium_framework/testcases.py
@@ -8,7 +8,17 @@ with open('config.json') as f:
 class SeleniumTestCase(object):
 
     def setup(self):
-        self.driver = webdriver.Firefox()
+        if config['use_sauce']:
+            desired_capabilities = getattr(webdriver.DesiredCapabilities, config['sauce_browser'])
+            self.driver = webdriver.Remote(
+                desired_capabilities=desired_capabilities,
+                command_executor="http://%s:%s@ondemand.saucelabs.com:80/wd/hub" % (
+                    config['sauce_username'], config['sauce_access_key']
+                )
+            )
+        else:
+            self.driver = webdriver.Firefox()
+
         self.driver.implicitly_wait(config['timeout'])
 
     def teardown(self):
